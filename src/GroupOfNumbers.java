6c4f1d93ce1d2186e5e66782f51b63283ee44080
91070160 <91070160@g.mvusd.net>
2020-04-06 09:47:50 -0700

    Initial commit

---
+++ src/GroupOfNumbers.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class GroupOfNumbers
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        int usernum;
+
+        System.out.println("Enter a list numbers: ");
+        usernum = scan.nextInt();
+
+        int [usernum] = new int [10];
+
+        System.out.println(usernum);
+    }
+}
